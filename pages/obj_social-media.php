9438e9f8058730e3a05219439c526b900833b90e
Kirk Daniels <kdee15@gmail.com>
2016-07-05 16:04:26 +0200

    resources

---
+++ pages/obj_social-media.php
@@ -0,0 +1,6 @@
+<ul class="social-media inline">
+    <li class="item"><a href="#" class="icon"><?php include 'assets/includes/icons/icon--fb.html';?></a></li>
+    <li class="item"><a href="#" class="icon"><?php include 'assets/includes/icons/icon--tw.html';?></a></li>
+    <li class="item"><a href="#" class="icon"><?php include 'assets/includes/icons/icon--li.html';?></a></li>
+    <li class="item"><a href="#" class="icon"><?php include 'assets/includes/icons/icon--yt.html';?></a></li>
+</ul>
---
+++ resources.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    
+        <script type="text/jscript" src="assets/js/bootstrap.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css">
+    </head>
+    <body class="page--resources">
+        
+        <?php include 'pages/obj_social-media.php';?>
+         
+    </body>
+</html>
